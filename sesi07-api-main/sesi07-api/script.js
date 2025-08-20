a2ece5bc1e7ba1852d48776996b15fc5a7b83c46
edsonvsantos <edsonvsantos.235@gmail.com>
2025-08-20 11:35:34 -0300

    Add files via upload

--- sesi07-api-main/sesi07-api/script.js
+++ sesi07-api-main/sesi07-api/script.js
@@ -3,10 +3,10 @@ const searchBtn = document.getElementById("searchBtn")
 const resultsDiv = document.getElementById("results")
 
 async function fetchCharacters(page){
-    resultsDiv.innerHTML = "<p>Carregando..<p>"
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
 
     try {
-        const response = await fetch('https://rickandmortyapi.com/api/character?page=${page}')
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         const data = await response.json()
         // console.log(data)
 
@@ -15,22 +15,21 @@ async function fetchCharacters(page){
             return
         }
 
-        resultsDiv.innerHTML = ""
-        data.results.forEach(character => {
+         resultsDiv.innerHTML = ""
+         data.results.forEach(character => {
             const card = document.createElement("div")
             card.className = "card"
             card.innerHTML = `
-                <img src="${character.image}"alt="${character.name}">
+                <img src="${character.image}" alt="${character.name}">
                 <h3>${character.name}</h3>
                 <p><strong>Status:</strong>${character.status}</p>
                 <p><strong>Espécie:</strong>${character.species}</p>
             `
             resultsDiv.appendChild(card)
-        })
-
+         })
     } catch (error) {
-        // console.log("Deu ruim")
-        resultsDiv.innerHTML = "<p>Erro ao buscar personagem!!!</p>"
+        // console.log("deu ruim")
+        resultsDiv.innerHTML = "<p>Erro ao buscar personagens!!!</p>"
     }
 }
 
